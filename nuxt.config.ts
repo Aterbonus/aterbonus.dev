1534b97b5f308292f129b57fe0724ee5b57be575
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2024-06-24 15:44:16 -0400

    feat: nuxt hub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
 	future: {
 		compatibilityVersion: 4
 	},
-	modules: ['@unocss/nuxt', '@vueuse/nuxt'],
+	modules: ['@unocss/nuxt', '@vueuse/nuxt', '@nuxthub/core'],
 	unocss: {
 		preflight: true
 	}
