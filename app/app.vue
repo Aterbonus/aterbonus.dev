a8555d477db6db1a3a87c25d3e390b010f3db360
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2024-06-24 22:32:28 -0400

    feat: nuxt seo

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup>
+
+</script>
+
 <template>
 	<div class="grid min-h-screen place-items-center supports-[(min-height:100svh)]:min-h-100svh">
 		<h1 class="text-center text-8 font-bold">
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,13 @@ export default defineNuxtConfig({
 	future: {
 		compatibilityVersion: 4
 	},
-	modules: ['@unocss/nuxt', '@vueuse/nuxt', '@nuxthub/core'],
+	modules: ['@unocss/nuxt', '@vueuse/nuxt', '@nuxthub/core', '@nuxtjs/seo'],
+	site: {
+		url: 'https://aterbonus.dev',
+		name: 'Aterbonus\' Home',
+		description: 'Aterbonus personal site',
+		defaultLocale: 'en' // not needed if you have @nuxtjs/i18n installed
+	},
 	unocss: {
 		preflight: true
 	}
