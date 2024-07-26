6d26880105d791722f5513dc18e9902b2c9dbeff
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2024-07-26 16:11:09 -0400

    fix: no index button

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,9 +2,6 @@
 	<div class="grid h-screen place-items-center container supports-[(height:100svh)]:h-100svh">
 		<h1 class="text-8 text-indigo font-bold">
 			En construcción ...
-			<Button @click="navigateTo('/dasdsa')">
-				Nos vamos a la B
-			</Button>
 		</h1>
 	</div>
 </template>
