a86291651fd091dbe335626f93678873f682b807
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2026-07-19 23:12:19 -0400

    fix: try nuxt 5

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,11 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2026-07-19',
-	future: {
-		compatibilityVersion: 5,
-	},
 	devtools: {
 		enabled: true,
 	},
 	modules: [
 		'@unocss/nuxt',
 		'@vueuse/nuxt',
-		'@pinia/nuxt',
 	],
 	css: [
 		'~/assets/css/main.css',
